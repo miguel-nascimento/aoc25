50bc9ae577928923995dfb841bc3c363330e4055
Miguel Nascimento <miguelgomes13@live.com>
2025-12-03 01:01:36 -0300

    chore: day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,67 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Debug)]
+enum Dir {
+    Left,
+    Right,
+}
+
+fn part1() -> i32 {
+    let input = File::open("./input/01.txt").unwrap();
+    let reader = BufReader::new(input);
+
+    let mut dial = 50;
+    let mut password = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let dir = if line.chars().nth(0).unwrap() == 'L' {
+            Dir::Left
+        } else {
+            Dir::Right
+        };
+        let val: i32 = line[1..].parse().unwrap();
+        match dir {
+            Dir::Left => dial = (dial - val).rem_euclid(100),
+            Dir::Right => dial = (dial + val).rem_euclid(100),
+        };
+        if dial == 0 {
+            password += 1
+        }
+    }
+    password
+}
+
+fn part2() -> i32 {
+    let input = File::open("./input/01.txt").unwrap();
+    let reader = BufReader::new(input);
+
+    let mut dial = 50;
+    let mut password = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let dir = if line.chars().nth(0).unwrap() == 'L' {
+            Dir::Left
+        } else {
+            Dir::Right
+        };
+        let val: i32 = line[1..].parse().unwrap();
+        let zeros = match dir {
+            Dir::Left => ((100 as i32 - dial).rem_euclid(100) + val) / 100,
+            Dir::Right => (dial + val) / 100,
+        };
+        password += zeros;
+        match dir {
+            Dir::Left => dial = (dial - val).rem_euclid(100),
+            Dir::Right => dial = (dial + val).rem_euclid(100),
+        };
+    }
+    password
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("part1: {}", part1());
+    println!("part2: {}", part2());
 }
